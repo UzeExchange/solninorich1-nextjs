e45efefabe2f5beae8c3088572833ff357e8783b
Uze Exchange <165286171+UzeExchange@users.noreply.github.com>
2024-04-05 16:32:12 +0000

    Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Banner from './components/Banner/index';
-import Work from './components/Work/index';
-import Companies from './components/Companies/index';
+import Banner from "./components/Banner/index";
+import Work from "./components/Work/index";
+import Companies from "./components/Companies/index";
 /* import Table from './components/Table/index'; */
 /* import Features from './components/Features/index'; */
 /* import Simple from './components/Simple/index'; */
-import Trade from './components/Trade/index';
-import Faq from './components/Faq/index';
-
+import Trade from "./components/Trade/index";
+import Faq from "./components/Faq/index";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       <Trade />
       <Faq />
     </main>
-  )
+  );
 }
 
 /* export default function Home() {
