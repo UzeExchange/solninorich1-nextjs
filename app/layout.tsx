e9f1287f41dd1276824fd4a641d71806db0dd18f
Uze Exchange <165286171+UzeExchange@users.noreply.github.com>
2024-04-05 16:38:26 +0000

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css';
-import '@/node_modules/react-modal-video/scss/modal-video.scss';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/index';
-
+import "./globals.css";
+import "@/node_modules/react-modal-video/scss/modal-video.scss";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/index";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: 'SOLNINO - $SRICH',
-  description: 'Powered by $SRICH Labs',
+  title: "SOLNINO - $SRICH",
+  description: "Powered by $SRICH Labs",
   icons: {
-    icon: '/logo.svg',
+    icon: "/logo.svg",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -25,5 +25,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
